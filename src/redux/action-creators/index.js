b28efd224cdcd7eec7bc4d04cb6e2d41f389333f
Inderpal Singh <92572473+99inder@users.noreply.github.com>
2022-12-08 19:50:27 +0530

    'fetchNotes' issue solved; fetch, add, delete note COMPLETED

--- src/redux/action-creators/index.js
+++ src/redux/action-creators/index.js
@@ -1,26 +1,79 @@
-export const fetchNotes = () => {
+const host = "http://localhost:5000";   //Hard Coding just for now
+const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjM4YTUyMjkzNWE0NDRmYjY3NzAyZGZjIn0sImlhdCI6MTY3MDA1ODgxN30.PbtPQ6-FJGCPfkdyfKeJanrsAghVZxfNyBhC_tCzoWM";   //Hard Coding just for now
+
+const notesFetched = (data) => {
     return {
-        type : "fetchNotes"
+        type: "notesFetched",
+        payload: data
     }
 }
 
-export const addNote = (data) => {
+const noteAdded = (data) => {
     return {
-        type : "addNote",
-        payload : data
+        type: "noteAdded",
+        payload: data
     }
 }
 
-export const deleteNote = (data) => {
+const noteDeleted = (data) => {
     return {
-        type : "deleteNote",
-        payload : data
+        type: "noteDeleted",
+        payload: data
     }
 }
 
 export const editNote = (data) => {
     return {
-        type : "editNote",
-        payload : data
+        type: "editNote",
+        payload: data
+    }
+}
+
+//Async action-creator to "FETCH ALL NOTES" from API
+export const fetchNotes = () => {
+    return async (dispatch) => {
+
+        const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'authToken': authToken,
+            }
+        });
+        const fetchedNotes = await response.json(); // parses JSON response into native JavaScript objects
+        dispatch(notesFetched(fetchedNotes));
+    }
+}
+
+//Async action-creator to "ADD A NOTE" via API call
+export const addNote = (data) => {
+
+    return async (dispatch) => {
+        const { title, description, tag } = data;     //destructuring payload for easy assignments
+        const response = await fetch(`${host}/api/notes/addnote`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authToken': authToken,
+            },
+            body: JSON.stringify({ "title": title, "description": description, "tag": tag })
+        });
+        const payload = await response.json(); // parses JSON response into native JavaScript objects
+        dispatch(noteAdded(payload));
+    }
+}
+
+//Async action-creator to 'DELETE A NOTE' via API call
+export const deleteNote = (data) => {
+
+    return async (dispatch) => {
+        await fetch(`${host}/api/notes/deletenote/${data}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'authToken': authToken,
+            }
+        });
+        dispatch(noteDeleted(data));
     }
 }
--- src/redux/reducers/notesReducer.js
+++ src/redux/reducers/notesReducer.js
@@ -1,64 +1,21 @@
 const initialState = [];
 
 const host = "http://localhost:5000";   //Hard Coding just for now
-const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjM4YTUyMjkzNWE0NDRmYjY3NzAyZGZjIn0sImlhdCI6MTY3MDA1ODgxN30.PbtPQ6-FJGCPfkdyfKeJanrsAghVZxfNyBhC_tCzoWM";   //Hard Coding just for now
+
 const notesReducer = (state = initialState, action) => {
 
     switch (action.type) {
 
         //ACTION: fetchNotes
-        case 'fetchNotes': {
+        case 'notesFetched': { return action.payload; }
 
-            //API CALL
-            (async () => {
-                const response = await fetch(`${host}/api/notes/fetchallnotes`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'authToken': authToken,
-                    }
-                });
-                const json = await response.json(); // parses JSON response into native JavaScript objects
-                console.log(json);
-                state = [...state, json];
-            })();
-
-            //Front End Logic
-            return state;
+        //ACTION: noteAdded
+        case 'noteAdded': {
+            return state = state.concat(action.payload);
         }
 
-        //ACTION: addNote
-        case 'addNote': {
-            //API CALL
-            const { title, description, tag } = action.payload;     //destructuring payload for easy assignments
-
-            (async () => {
-                const response = await fetch(`${host}/api/notes/addnote`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'authToken': authToken,
-                    },
-                    body: JSON.stringify({ "title": title, "description": description, "tag": tag })
-                });
-                const json = await response.json(); // parses JSON response into native JavaScript objects
-            })();
-
-            //Front End Logic
-            const note = {
-                "_id": "638db8b23ee64dg81b84",
-                "user": "638a522935a444fb67702dfc",
-                "title": action.payload.title,
-                "description": action.payload.description,
-                "tag": action.payload.tag,
-                "date": "2022-12-05T09:24:02.954Z",
-                "__v": 0
-            };
-            return state = state.concat(note);
-        };
-
         //ACTION: deleteNote
-        case 'deleteNote': {
+        case 'noteDeleted': {
             //TODO: API CALL
             const newNoteState = state.filter((note) => { return note._id !== action.payload; });
             state = newNoteState;
@@ -78,6 +35,7 @@ const notesReducer = (state = initialState, action) => {
                     },
                     body: JSON.stringify({ "title": title, "description": description, "tag": tag }) // body data type must match "Content-Type" header
                 });
+                // eslint-disable-next-line
                 const json = await response.json(); // parses JSON response into native JavaScript objects
             })();
 
